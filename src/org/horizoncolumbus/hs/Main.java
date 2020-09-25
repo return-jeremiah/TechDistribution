7ec13b7befb0e43434ddcd1b8a23644782fcd224
HSA-Student <student.jr.coleman@horizoncolumbus.org>
2020-09-25 12:43:06 -0400

    Boolean Logic

---
+++ src/org/horizoncolumbus/hs/Main.java
@@ -0,0 +1,22 @@
+package org.horizoncolumbus.hs;
+
+public class Main {
+
+    public static void main(String[] args) {
+// write your code here
+        boolean isJunior = false; // true // false
+        boolean isSenior = false;
+        boolean isTeacher = false;
+        boolean isAdmin = false;
+        boolean isCTE = false;
+        boolean isHome = false;
+
+        if (isJunior || (isSenior && isCTE)) {
+            System.out.println("Give Windows Laptop");
+        } else if (isTeacher || (isAdmin && isHome)) {
+            System.out.println("Give MacBook");
+        } else{
+            System.out.println("Give Chromebook");
+        }
+    }
+}
